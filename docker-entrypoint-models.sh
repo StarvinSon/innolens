5fe403f33fc4c47a9769e4cec92fa3086bf37c62
Alvin Leung <alvinleung1996@gmail.com>
2020-08-13 17:14:49 +0000

    Enhanced docker files

--- docker-entrypoint-models.sh
+++ docker-entrypoint-models.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-if [ ${#} -eq 0 ] || [ "${1#-}" != "${1}" ]; then
+if [ "${#}" -eq 0 ] || [ "${1#-}" != "${1}" ]; then
   set -- python -m innolens_models serve "$@"
 fi
 
--- docker-entrypoint-server.sh
+++ docker-entrypoint-server.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-if [ ${#} -eq 0 ] || [ "${1#-}" != "${1}" ]; then
+if [ "${#}" -eq 0 ] || [ "${1#-}" != "${1}" ]; then
   set -- node ./packages/server/out/index.js start "$@"
 fi
 
